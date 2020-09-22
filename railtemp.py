6ef5abc8f63d23a3c045a27f86566e5eb9b2d774
ary vinicius <aryviniciusnf@gmail.com>
2020-09-22 20:14:16 +0100

    classes added

---
+++ railtemp.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+
+
+class Rail:
+    ''' 
+    Class that defines rail geometric properties and location
+    '''
+    def __init__(self):
+        return
+
+
+class RailMaterial:
+    '''
+    Class to define rail material properties
+    '''
+    def __init__(self):
+        return
+
+class Simu:
+
+    def __init__(self):
+        return
+
+class InputData:
+
+
+    def __init__(self):
+
+        return
+
