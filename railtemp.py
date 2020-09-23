4b22179a0e65ec665940189f34c4ef3fc3919763
ary vinicius <aryviniciusnf@gmail.com>
2020-09-23 11:18:56 +0100

    Rail class created

--- railtemp.py
+++ railtemp.py
@@ -7,9 +7,47 @@ from math import *
 class Rail:
     ''' 
     Class that defines rail geometric properties and location
+     
+    Params:
+    name: name of the rail cross section, default=UIC42, string;
+    azimuth: azimuth of the rail track, between 0 and 180 [degrees], float64;
+    lat,long: latitude and longitude of the rail track [degrees], float64;
+    cross_area: cross section area [m²], float64;
+    convection_area: area that exchange heat by convection [m²], flota64;
+    radiation_area: area that exchange heat by radiation [m²], float64;
+    material: RailMaterial object
+        
     '''
-    def __init__(self):
-        return
+    def __init__(self,name,azimuth,lat,long,cross_area,convection_area,radiation_area,material):
+        
+        if isinstance(material,RailMaterial):
+            pass
+        else:
+            print('invalid rail material')
+            return
+        if 0<=azimuth<=180:
+            pass
+        else:
+            print('invalid azimuth. It must be between 0-180')
+            return
+        
+        
+        
+        self.name = name
+        self.azimuth = azimuth
+        self.position = (lat,long)
+        self.cross_area = cross_area
+        self.convection_area = convection_area
+        self.radiation_area = radiation_area
+        self.material = material
+        self.volume = self.cross_area
+        
+        pass
+
+    
+
+
+        
 
 
 class RailMaterial:
@@ -19,12 +57,12 @@ class RailMaterial:
 
     def __Cr(temperature):
         '''
-        Specific heat deppeding on the temperature according to EN1993-1-2
+        Specific heat depending on the temperature according to EN1993-1-2
 
-        params:
+        Params:
         temperature [Celsius], float64
 
-        returns:
+        Returns:
         specific heat [J/ (kg K)]
         '''
         t = temperature
@@ -37,7 +75,7 @@ class RailMaterial:
 
     def __init__(self,density=7850,solar_absort=0.8,emissivity=0.7, specific_heat=__Cr):
         '''
-        Initiante a RailMaterial instance
+        Create a RailMaterial instance
 
         params:
 
@@ -62,10 +100,7 @@ class RailMaterial:
         self.emissivity = emissivity
         self.specific_heat = specific_heat
 
-    
-    
-
-            
+             
             
 
 
